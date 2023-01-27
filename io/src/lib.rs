2db22bd1bb34417008943b974785074dee995df0
b3zrazli4n0 <b3zrazli4n0@gmail.com>
2023-01-27 23:24:25 +0300

    feat: update deps, fix metahash

--- io/src/lib.rs
+++ io/src/lib.rs
@@ -7,7 +7,7 @@ pub struct DemoPingMetadata;
 
 impl Metadata for DemoPingMetadata {
     type Init = ();
-    type Handle = InOut<Vec<u8>, Vec<u8>>;
+    type Handle = InOut<String, String>;
     type Others = ();
     type Reply = ();
     type Signal = ();
